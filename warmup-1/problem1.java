085e8ce15348c8c3724c18820c442465d9cff852
Ibraheem Esayed <ibraheeme2024@gmail.com>
2023-07-05 11:12:14 -0400

    finished problem 1

--- warmup-1/problem1.java
+++ warmup-1/problem1.java
@@ -1,6 +1,9 @@
 public boolean sleepIn(boolean weekday, boolean vacation) {
-  if (!weekday || vacation) {
-    return true;
-  }
+  
+   if (!weekday || vacation) {
+     return true;
+  } 
   return false;
+  
 }
+
